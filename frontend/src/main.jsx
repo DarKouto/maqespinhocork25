a6107f9fb66c96c6904d613a0e3d9a66ad853802
DarKouto <danielcouto9@live.com>
2025-07-11 12:09:54 +0100

    refactored main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-// Importa o ChakraProvider E extendTheme do pacote principal @chakra-ui/react
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'; // <--- AMBOS AQUI!
-
-// Cria um tema básico. O extendTheme({}) usa o tema padrão como base.
-const customTheme = extendTheme({}); // <--- Cria um tema mesmo que vazio
+import { ChakraProvider } from '@chakra-ui/react'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* Passa o tema customTheme para o ChakraProvider */}
-    <ChakraProvider theme={customTheme}> {/* <--- USA O TEMA CRIADO */}
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>,
