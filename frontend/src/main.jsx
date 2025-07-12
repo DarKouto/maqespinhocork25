fae878ac98b34eac75cee38049e1e61ad22a75b3
DarKouto <danielcouto9@live.com>
2025-07-12 14:00:53 +0100

    code refactor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 
-// Importa o ThemeProvider e o createTheme do Material UI
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-// Cria um tema Material UI padrão
 const theme = createTheme();
 
 createRoot(document.getElementById('root')).render(
