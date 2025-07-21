76509c0b37d4a55222ead13cfcea22f207bb4135
DarKouto <danielcouto9@live.com>
2025-07-21 19:23:37 +0100

    minor refactor on footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,31 +1,30 @@
 import { Box, Typography, Link } from '@mui/material';
 
 function Footer() {
-  const currentYear = new Date().getFullYear(); // Obtém o ano atual dinamicamente
-  const githubLink = "https://github.com/DarKouto";
+  const currentYear = new Date().getFullYear();
 
   return (
     <Box
-      component="footer" // Indica que este Box representa um elemento footer semântico
+      component="footer" 
       sx={{
         width: '100%',
         mt: 8, // Margem superior para separar do conteúdo acima
         py: 3, // Padding vertical
         px: 2, // Padding horizontal
         backgroundColor: 'primary.main', // Cor de fundo do Material UI
-        color: 'white', // Cor do texto
+        color: 'white',
         textAlign: 'center',
-        boxShadow: '0px -2px 4px rgba(0, 0, 0, 0.2)', // Sombra para dar um pouco de profundidade
+        boxShadow: '0px -2px 4px rgba(0, 0, 0, 0.2)',
       }}
     >
       <Typography variant="body2">
         Copyright © {currentYear}: MaqEspinhoCork.com - {' '}
         <Link
-          href={githubLink}
+          href="https://github.com/DarKouto"
           color="inherit" // Mantém a cor do texto do footer
-          underline="hover" // Sublinha apenas ao passar o rato
-          target="_blank" // Abre o link numa nova aba
-          rel="noopener noreferrer" // Prática de segurança para links externos
+          underline="hover"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Desenvolvido por Daniel Couto (DarKouto)
         </Link>
