8844d384ced81212da1607f5c970dae0555b9593
DarKouto <danielcouto9@live.com>
2025-07-24 19:14:01 +0100

    create routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography, Toolbar } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppBar from './components/AppBar'
 import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
@@ -6,28 +7,33 @@ import Contactos from './components/Contactos'
 
 function App() {
   return (
-    <>
+    <Router>
       <Box sx={{ flexGrow: 1 }}>
 
         <AppBar />
         <Toolbar />
 
-        <Contactos />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <Box sx={{ p: 3 }}>
+                <Typography variant="h4" gutterBottom>
+                  Bem-vindo à MEC: MaqEspinhoCork v2025!
+                </Typography>
+                <Typography variant="body1" component="p">
+                  O site está a ganhar forma. Esta é a área de conteúdo principal.
+                </Typography>
+              </Box>
+            </>
+          }
+          />
+          <Route path="/contactos" element={<Contactos />} />
+        </Routes>
 
-        <HeroSection /> {/* <--- Adiciona a tua Hero Section aqui */}
-
-        <Box sx={{ p: 3 }}>
-          <Typography variant="h4" gutterBottom>
-            Bem-vindo à MEC: MaqEspinhoCork v2025!
-          </Typography>
-
-          <Typography variant="body1" component="p">
-            O site está a ganhar forma. Esta é a área de conteúdo principal.
-          </Typography>
-        </Box>
         <Footer />
       </Box>
-    </>
+    </Router>
   )
 }
 
