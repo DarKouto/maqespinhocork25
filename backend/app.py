8745b8fedd15a71749bbaf30f47ce837463ffc78
DarKouto <danielcouto9@live.com>
2025-08-11 14:27:49 +0100

    create contactos route

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,25 @@
-# Importa a classe Flask
+# Vai à pasta venv buscar o Flask e jsonify que instalei
 from flask import Flask, jsonify
 
-# Cria uma instância da aplicação Flask
+# Cria uma instância da classe flask (modelo) na variável app
 app = Flask(__name__)
 
-# Cria a rota principal
+# Sempre que a rota '/' for pedida ao servidor, a função home é executada
 @app.route('/')
 def home():
     return jsonify({"message": "Olá, o teu servidor backend está a funcionar!"})
 
-# Executa a aplicação
+# Sempre que a rota '/contactos' for pedida ao servidor, a função contactos é executada
+@app.route('/contactos')
+def contactos():
+    return "Estás na página contactos"
+
+@app.route('/contactos', methods=['POST'])
+def handle_contact_form():
+    # A lógica para processar o formulário vai ficar aqui
+    # ...
+    return "Dados recebidos!"
+
+# Corre a aplicação
 if __name__ == '__main__':
-    app.run(debug=True)
-    # O debug=True é para o ambiente de desenvolvimento.
-    # Ele reinicia o servidor automaticamente quando fazes alterações.
+    app.run(debug=True) # o debug=True serve para o servidor reiniciar automaticamente quando mudo o código
