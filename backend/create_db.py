201923d1ac1099238eb1be56bde3cc4a79b742d4
DarKouto <danielcouto9@live.com>
2025-10-21 11:53:30 +0100

    add create_db.py

---
+++ backend/create_db.py
@@ -0,0 +1,8 @@
+# create_db.py - é usado uma única vez no terminal com: "python create_db.py"
+# serve para criar as tabelas da base de dados com base no ORM do models.py
+from app import app, db
+from models import Maquinas, Imagens, Utilizador 
+
+with app.app_context():
+    db.create_all() 
+    print("Tabelas criadas no Neon Postgres (ou já existiam).")
